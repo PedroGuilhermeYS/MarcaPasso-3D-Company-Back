3d40e0dd9148884cbfbd6b35bac98bef49ad3e9e
PedroGuilhermeYS <pedro210905@gmail.com>
2026-02-27 19:00:43 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ifpe/MarcaPasso3D/MarcaPasso3DApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.MarcaPasso3D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaPasso3DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaPasso3DApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpe/MarcaPasso3D/MarcaPasso3DApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.MarcaPasso3D;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcaPasso3DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
